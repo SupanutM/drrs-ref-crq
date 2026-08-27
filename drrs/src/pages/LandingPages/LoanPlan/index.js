e2d3c1e33df2a586c5be4e60309a1f1463a99810
SupanutM <supanut4546@gmail.com>
2026-08-27 14:18:07 +0700

    🐛: แก้ลิงก์และชื่อปุ่มบน navbar จาก NDRS เป็น DRRS
    ปุ่มมุมขวาบนของทุกหน้าเคยชี้ไป https://ln15.gsb.or.th/ndrs
    ซึ่งเป็นระบบคนละตัว (มาตรการช่วยลูกหนี้ไม่เกิน 1 แสนบาท)
    และป้ายปุ่มเขียนว่า "GSB NDRS Website"
    
    เปลี่ยนเป็น https://ln15.gsb.or.th/drrs และ "GSB DRRS Website"
    ให้ตรงกับระบบนี้ (path /drrs จะใช้งานได้เมื่อ prod ขึ้น)
    
    แก้ 7 หน้า: Consent, GenContract, LoanPlan, LoanPlanDetails,
    PlanPreview, PlanSummary, SelectPlan

--- drrs/src/pages/LandingPages/LoanPlan/index.js
+++ drrs/src/pages/LandingPages/LoanPlan/index.js
@@ -44,8 +44,8 @@ function LoanPlan() {
         routes={routes}
         action={{
           type: "external",
-          route: "https://ln15.gsb.or.th/ndrs",
-          label: "GSB NDRS Website",
+          route: "https://ln15.gsb.or.th/drrs",
+          label: "GSB DRRS Website",
           color: "default",
         }}
         backRoute
--- drrs/src/pages/LandingPages/SelectPlan/index.js
+++ drrs/src/pages/LandingPages/SelectPlan/index.js
@@ -38,8 +38,8 @@ function SelectPlan() {
                 routes={routes}
                 action={{
                     type: "external",
-                    route: "https://ln15.gsb.or.th/ndrs",
-                    label: "GSB NDRS Website",
+                    route: "https://ln15.gsb.or.th/drrs",
+                    label: "GSB DRRS Website",
                     color: "default",
                 }}
                 backRoute
