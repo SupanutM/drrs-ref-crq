979a4bd543763a3849ed277af837e72207378b72
SupanutM <supanut4546@gmail.com>
2026-09-18 11:12:11 +0700

    🔧: update web.config

--- drrs/public/config.js
+++ drrs/public/config.js
@@ -1,8 +1,6 @@
 window.APP_CONFIG = {
-  BACKEND_URL: "http://localhost:5000",
-  // BACKEND_URL=http://10.22.51.190/drrs-api,
-  // BACKEND_URL=http://192.168.52.248/drrs-api,
-  SESSION_TIMEOUT: 120,
+  BACKEND_URL: "/drrs/reword",
+  SESSION_TIMEOUT: 900,
   SESSION_WARNING: 15,
   MAX_CONNECTIONS: 5,
 };
